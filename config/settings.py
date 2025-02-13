df22d66cb6a75b5a18765ec5e3b3bb9621d6f107
KIMMINJI <amber_lucy@naver.com>
2025-02-13 19:53:48 +0900

    임포트

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
