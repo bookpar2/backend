cef287bb1872769e0fa1629cdefc5e4e1b81126f
KIMMINJI <amber_lucy@naver.com>
2025-02-27 08:41:47 +0900

    fix : 이미지 url로 변경, 상태 추가

--- book/models.py
+++ book/models.py
@@ -2,19 +2,25 @@ from django.db import models
 from users.models import User
 
 class Book(models.Model):
+    STATUS_CHOICES = [
+        ('FOR_SALE', '판매 중'),
+        ('IN_PROGRESS', '거래 중'),
+        ('COMPLETED', '거래 완료'),
+    ]
+
     title = models.CharField(max_length=50)  # 책 제목
     author = models.CharField(max_length=50)  # 저자
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # 가격
+    price = models.IntegerField()  # 가격
     description = models.TextField(blank=True, null=True)  # 설명
-    image = models.ImageField(upload_to='bookpar2/book/', blank=True, null=True)  # 책 이미지
+    image_url = models.CharField(max_length=200, blank=True, null=True)  # S3 이미지 URL
+    major = models.CharField(max_length=50)  # 전공
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='FOR_SALE')  # 거래 상태
     created_at = models.DateTimeField(auto_now_add=True)  # 등록 시간
     updated_at = models.DateTimeField(auto_now=True)  # 수정 시간
-    major = models.TextField(max_length=50)  # 전공
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # 사용자 정보
-    
+
     def __str__(self):
         return self.title
-    # 유저 추가하기
 
 # Create your models here.
 # 데이터 베이스(수납장)을 만드는 공간
