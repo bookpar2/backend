0e37652ea3ca6acad22b4022ec65609697a505a6
KIMMINJI <amber_lucy@naver.com>
2025-02-27 06:50:24 +0900

    fix : 파일 수정

--- config/asgi.py
+++ config/asgi.py
@@ -11,15 +11,17 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns  #WebSocket URL 라우팅 추가
+from chat.routing import websocket_urlpatterns  # WebSocket URL 라우팅 추가
 
+# Django 설정 모듈을 환경 변수로 설정
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
+# ASGI 애플리케이션 설정
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  #HTTP 요청 처리
-    "websocket": AuthMiddlewareStack(  #WebSocket 요청 처리
+    "http": get_asgi_application(),  # HTTP 요청 처리
+    "websocket": AuthMiddlewareStack(  # WebSocket 요청 처리
         URLRouter(
-            websocket_urlpatterns  #WebSocket URL을 Django에 연결
+            websocket_urlpatterns  # WebSocket URL을 Django에 연결
         )
     ),
 })
