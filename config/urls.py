f8e7b3f0538df8b6ba3f355f888a09d237b71d28
KIMMINJI <amber_lucy@naver.com>
2025-02-28 01:08:50 +0900

    .

--- config/urls.py
+++ config/urls.py
@@ -16,13 +16,13 @@
 # """
 from django.contrib import admin
 from django.urls import path, include
-from book.views import BookListCreateView, BookDetailView
+from book.views import BookListCreateView, BookDetailView, BookListByUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/books/', BookListCreateView.as_view(), name='book-list-create'),
     path('api/v1/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('api/v1/books/user/', BookListCreateView.as_view(), name='book-by-user'),
+    path('api/v1/books/user/', BookListByUser.as_view(), name='book-by-user'),
     path('api/v1/users/', include('users.urls')),
     path('', include('chat.urls')),
 ]
