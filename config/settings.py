62cdbca72e77ef6892ae768acbd6d3a78e3b9a47
parksooyong03 <parkwaterdragon@tukorea.ac.kr>
2025-02-28 04:38:48 +0900

    테스트 코드 5차 수정

--- config/settings.py
+++ config/settings.py
@@ -82,6 +82,12 @@ MIDDLEWARE = [
 
 # CORS 설정
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
+]
+
 
 # JWT 설정
 REST_FRAMEWORK = {
