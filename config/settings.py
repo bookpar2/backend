878d57a9bd54724fa181061397ec2cc862cc7f1d
KIMMINJI <amber_lucy@naver.com>
2025-03-05 21:52:47 +0900

    fix : 설정 추가

--- config/settings.py
+++ config/settings.py
@@ -95,6 +95,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.parsers.MultiPartParser'
     ),
 }
 
