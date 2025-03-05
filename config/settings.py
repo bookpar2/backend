ca3b99659f401cea6e10590ff820f9df57341472
KIMMINJI <amber_lucy@naver.com>
2025-03-05 22:31:45 +0900

    fix : 토큰 필수 제거

--- config/settings.py
+++ config/settings.py
@@ -96,9 +96,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
 }
 
 SIMPLE_JWT = {
